a1ac85c115c3dc17eae56e152a5f7c2865c72613
m00k <christian.bican@gmail.com>
2020-08-02 05:13:18 +0200

    initial commit

---
+++ precache-manifest.e1e3aeff3e933eef4e761f04f4ba70d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9cf17099b0f39bbfc6477328db1eee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e6b864cef139eb70275",
+    "url": "/static/js/2.8308da6c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8308da6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a60591d44677f182fb5",
+    "url": "/static/js/main.19af4018.chunk.js"
+  },
+  {
+    "revision": "26d70e804cd5d8c158ef",
+    "url": "/static/js/runtime-main.cee2ac7f.js"
+  }
+]);
